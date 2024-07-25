8901eb6fb25b0bae601062a74cfce02dbf4f26dd
notkath <148864502+notkath@users.noreply.github.com>
2024-07-25 20:31:22 +0530

    Sd_Card

---
+++ src/main.cpp
@@ -0,0 +1,78 @@
+#include <Arduino.h>
+
+ 
+#include <SD.h>
+#include <SPI.h>
+
+File myFile;
+
+// change this to match your SD shield or module;
+// Arduino Ethernet shield: pin 4
+// Adafruit SD shields and modules: pin 10
+// Sparkfun SD shield: pin 8
+// Teensy audio board: pin 10
+// Teensy 3.5 & 3.6 & 4.1 on-board: BUILTIN_SDCARD
+// Wiz820+SD board: pin 4
+// Teensy 2.0: pin 0
+// Teensy++ 2.0: pin 20
+const int chipSelect = BUILTIN_SDCARD;
+
+void setup()
+{
+ //UNCOMMENT THESE TWO LINES FOR TEENSY AUDIO BOARD:
+ //SPI.setMOSI(7);  // Audio shield has MOSI on pin 7
+ //SPI.setSCK(14);  // Audio shield has SCK on pin 14
+  
+ // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+   while (!Serial) {
+    ; // wait for serial port to connect.
+  }
+
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(chipSelect)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+  
+  // open the file. 
+  myFile = SD.open("test.csv", FILE_WRITE);
+  
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to test.txt...");
+    myFile.println("a,b,c");
+    myFile.println("1,2,3");
+
+	// close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+  
+  // re-open the file for reading:
+  myFile = SD.open("test.csv");
+  if (myFile) {
+    Serial.println("test.csv:");
+    
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+    	Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+  	// if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
+
+void loop()
+{
+	// nothing happens after setup
+}
